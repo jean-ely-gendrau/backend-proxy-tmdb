295d86dd9202ea8a92b2310f2fccc81b3826b1c2
jean-ely.gendrau <150784008+jean-ely-gendrau@users.noreply.github.com>
2026-01-29 16:36:21 +0100

    Ajout de la configuration du serveur et de l'API TMDB pour récupérer les films populaires

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express"
+import "dotenv/config"
+
+const app = express();
+
+const BASE_URL = process.env.BASE_URL;
+const API_TOKEN = process.env.API_TOKEN;
+
+app.get("/api/tmdb/movie/popular", async (req, res) => {
+
+    const url = new URL(`${BASE_URL}/movie/popular?language=fr-FR`)
+
+    try {
+        const fetchAPI = await fetch(url, {
+            headers: {
+                Authorization: ` Bearer ${API_TOKEN}`,
+                accept: 'application/json'
+            }
+        });
+
+        if (!fetchAPI.ok) throw new Error("Erreur TMDB" + fetchAPI.status);
+
+        const response = await fetchAPI.json();
+
+        res.json(response);
+    } catch (error) {
+        return res.status(500).json({ error: "Erreur proxy TMDB " + error.message });
+    }
+
+});
+
+app.listen(3500, () => {
+    console.log("Serveur démarrer sur le port http://localhost:3500");
+});
